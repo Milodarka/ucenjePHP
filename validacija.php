8202e7d8e1ed7a2e12108b92ad492ef0a539886b
Milodarka <plavemisli@gmail.com>
2019-07-26 14:04:10 +0200

    validacije  26.7

--- validacija.php
+++ validacija.php
@@ -1,45 +1,179 @@
 <html>
+<head>
+        <style>
+            .error 
+            {
+                color:red;
+            }
+            body 
+            {
+                background-color:pink;
+            }
+        </style>
+</head>
     <body>
+<?php
+    $ime = $prezime = $email = $sajt= $komentar = $pol = $pravila=  "";
+   $imeO= $prezimeO = $emailO = $sajtO = $komentarO = "*";
+   
+
+    if($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        if(empty($_POST["ime"])==TRUE)
+        {
+            $imeO = "Ime ne sme biti prazno polje";
+        }
+        elseif(preg_match("[a-z A-Z]",$_POST["ime"]) == FALSE)
+
+        {
+            $imeO = "Ime moze sadrzati samo slova";
+        }
+        else 
+        {
+            $ime= $_POST["ime"];
+        }
+
+
+        
+        if(empty($_POST["prezime"])==TRUE)
+        {
+            $prezimeO = "Prezime ne sme biti prazno polje";
+        }
+        elseif(preg_match("[a-z A-Z]",$_POST["prezime"]) == FALSE)
+
+        {
+            $prezimeO = "Prezime moze sadrzati samo slova";
+        }
+        else 
+        {
+            $prezime= $_POST["prezime"];
+        }
+
+        
+        if(empty($_POST["email"])==TRUE)
+        {
+            $emailO = "Mejl ne sme biti prazno polje";
+        }
+        elseif(filter_var($_POST["email"], FILTER_VALIDATE_EMAIL) == FALSE)
+        {
+            $emailO = " *emajl nije dobro unet";
+        }
+        else 
+        {
+            $email= $_POST["email"];
+        }
+
+           
+
+        if(empty($_POST["sajt"])==TRUE)
+        {
+            $sajtO = "Sajt ne sme biti prazno polje";
+        }
+        
+        elseif(filter_var($_POST["sajt"], FILTER_VALIDATE_URL) == FALSE)
+            {
+                $sajtO = "Neispravno unet website";
+            }
+        else 
+        {
+            $sajt= $_POST["sajt"];
+        }
+           
+        
+
+        if(strlen($_POST["komentar"])<5)
+        {
+            $komentarO = "Komentar mora imati vise karaktera";
+        }
+        else 
+        {
+            $komentar= $_POST["komentar"];
+        }
+        
+        $pol = $_POST["pol"];
+
+        if(isset($_POST["pravila"]) == TRUE)
+        {
+            $pravila = "Pravila su prihvacena";
+        }
+        else 
+        {
+            $pravila = "Pravila nisu prihvacena";
+        }
+
+        
+    }
+    
+
+
+
+
+?>
+
     <h1> Primer PHP validacije forme</h1>
 
-        <form action="<?php echo htmlspecialcharacters($_SERVER["PHP_SELF"]);?>"
-        method = "post">
+        <form method="post" action="validacija.php">
 
             <label> Ime: </label>
-                <input type="text"  name = "Ime"> <br>
+            
+                <input type="text"  name = "ime"> 
+                <span class = "error"> <?php echo $imeO; ?> </span>
+                <br>
             <br>
             <label> Prezime: </label>
-                <input type="text"  name = "Prezime"> <br>
+                <input type="text"  name = "prezime">
+                <span class = "error"> <?php echo $prezimeO; ?> </span>
+                <br>
             <br>
             <label> e-mail: </label>
-                <input type= "text" name = "mail"> <br>
+                <input type= "text" name = "email"> 
+                <span class = "error"> <?php echo $emailO; ?> </span>
+                <br>
             <br>
             <label> Sajt: </label>
-                <input type="url"  name = "sajt"> <br>
+                <input type="text"  name = "sajt">
+                <span class = "error"> <?php echo $sajtO; ?> </span>
+                <br>
             <br>
             <label> Komentar: </label>
-                <textarea name = "komentar" form = "usrform"> 
-                Unesite svoj komentar 
-                </textarea> <br>
+                <textarea name = "komentar" > 
+               
+                </textarea>
+                <span class = "error"> <?php echo $komentarO; ?> </span>
+                 <br>
             <br>
             <label> Pol </label> 
-            <input type= "radio" name="pol" value= "Ostalo"> Ostalo
-            <input type= "radio" name= "pol" value ="Zenski"> Zenski
-            <input type= "radio" name="pol" value ="Muski"> Muski
+                <input type= "radio" name="pol" value= "Ostalo" checked> Ostalo
+                <input type= "radio" name= "pol" value ="Zenski"> Zenski
+                <input type= "radio" name="pol" value ="Muski"> Muski
                <br>
                <br>
-               <input type= "submit" value ="potvrdi">
-                <br> 
+            Prihvatate pravila koriscenja
+            <input type="checkbox" name="pravila" value="da">
+            <br>
+            <br>
+            <input type= "submit" name="submit" value ="potvrdi">
+            <br> 
 
         </form>
- 
+ <img src = "smrda.jpg" width= "30%">
     
 <?php
-echo " <p> Vas unos </p>";
+echo " <h2> Vas unos </h2>";
 echo "<br>";
-echo "Vase ime i prezime je :" . $_POST["Ime"] . "" .$_POST["Prezime"];
+echo "Korisnik :" . $ime . "" . $prezime;
 echo "<br>";
-echo "Vase Email je :" . $_POST["mail"];
+echo "Mejl korisnika :" . $email ;
+echo "<br>";
+echo "Website korisnika :" . $sajt ;
+echo "<br>";
+echo "Komentar korisnika:" . $komentar;
+echo "<br>";
+echo "Pol korisnika" . $pol;
+echo "<br>";
+echo "Prihvacena pravila koriscenja:" . $pravila;
+echo "<br>";
+
 ?>
         
     </body>
